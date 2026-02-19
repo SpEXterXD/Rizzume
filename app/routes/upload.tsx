8f36343dcc7a8a9a2964bafc2f9ad4ee783d173b
SpEXterXD <nirmalyapal404@gmail.com>
2026-02-20 05:09:51 +0530

    Resume Feedback Page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -79,6 +79,7 @@ const upload = () => {
     await kv.set(`resume:${uuid}`, JSON.stringify(data));
     setStatusText("Analysis Complete! Redirecting...");
     console.log(data);
+    navigate(`/resume/${uuid}`);
   };
 
   const handelSubmit = (e: FormEvent<HTMLFormElement>) => {
